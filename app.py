5836d55f3f1f365156f7a7694f34431d130a7356
AlainLobato <lobatoa829@gmail.com>
2024-11-06 12:53:31 -0600

    Plots and fix routes

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import datetime
+from decimal import Decimal
 from flask import Flask, render_template, request, redirect, url_for, flash
 import pyodbc
+import plotly.graph_objs as go
+import json
+import urllib.parse
+
+NOMBRE = 2
+GASTO = 3
 
 app = Flask(__name__)
 
@@ -16,6 +24,28 @@ connection_string = (
 )
 
 mysql = pyodbc.connect(connection_string)
+
+""" CREATE DATABASE SaveMyMoney
+
+CREATE TABLE TipoGasto(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Tipo VARCHAR(40) NOT NULL,
+	Descripcion VARCHAR(100)
+)
+
+CREATE TABLE Gastos (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    TipoID INT NOT NULL,
+    Nombre VARCHAR(100) NOT NULL,
+    Gasto DECIMAL(10,2) NOT NULL,
+	Fecha DATE NOT NULL DEFAULT GETDATE(),
+    CONSTRAINT FK_Gastos_TipoGasto FOREIGN KEY (TipoID) REFERENCES TipoGasto(ID)
+)
+
+SELECT * FROM Gastos
+SELECT * FROM TipoGasto """
+
+
 
 
 #Settings
@@ -144,5 +174,70 @@ def eliminarG(id):
     flash('Gasto eliminado con exito')
     return redirect(url_for('gastos', id=ID[0].TipoID))
 
+@app.route('/graficos/')
+def graficos():
+    cursor = mysql.cursor()
+    cursor.execute('SELECT * FROM TipoGasto')
+    tipos = cursor.fetchall()
+    return render_template('Graficos.html', tipo = tipos)
+
+def serialize_data(data):
+    rows = [[e for e in element] for element in data]
+    for i, row in enumerate(rows):
+        for j, element in enumerate(row):
+            if type(element) == Decimal:
+                rows[i][j] = float(element)
+            elif type(element) == datetime.date:
+                element: datetime.date = element
+                rows[i][j] = element.strftime("%d-%m-%Y, %H:%M:%S")
+    return rows
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    TIPO = request.form['Tipo']
+    DISTRO = request.form['Distro']
+    cursor = mysql.cursor()
+    cursor.execute('SELECT * FROM TipoGasto WHERE TIPO = ?', TIPO)
+    ID = cursor.fetchall()
+
+    if DISTRO == 'Individual':
+        cursor.execute('SELECT * FROM Gastos WHERE TipoID = ?', ID[0].ID)
+        data = cursor.fetchall()
+
+    elif DISTRO == 'Mensual':
+        MES = request.form['Mes']
+        cursor.execute('SELECT * FROM Gastos WHERE TipoID = ? AND YEAR(Fecha) = YEAR(GETDATE()) AND MONTH(Fecha) = ?', ID[0].ID, MES)
+        data = cursor.fetchall()
+
+    elif DISTRO == 'Anual':
+        AÑO = request.form['Anio']
+        cursor.execute('SELECT * FROM Gastos WHERE TipoID = ? AND YEAR(Fecha) = YEAR(?)', ID[0].ID, AÑO)
+        data = cursor.fetchall()
+    
+    data_ = serialize_data(data)
+    json_data = json.dumps(data_)
+
+    return redirect(url_for('graphic', info=json_data))
+
+@app.route('/graphic/<info>')
+def graphic(info):
+    decoded_json_data = urllib.parse.unquote(info)
+    info_data = json.loads(decoded_json_data)
+    
+    info_x = [i[NOMBRE] for i in info_data]
+    info_y = [i[GASTO] for i in info_data]
+
+    graph1, graph2 = create_plot(info_x, info_y)
+
+    return render_template('newGraph.html', graphic_1=graph1, graphic_2 = graph2)
+
+def create_plot(x_data, y_data):
+    fig = go.Figure(data=[go.Bar(x=x_data, y=y_data)])
+    fig2 = go.Figure(data=[go.Pie(labels=x_data, values=y_data)])
+    graph_1 = fig.to_html(full_html=False)
+    graph_2 = fig2.to_html(full_html=False)
+    
+    return graph_1, graph_2
+
 if(__name__ == '__main__'):
-    app.run(port=3000, debug = True)
+    app.run(port=5000, debug = True)
